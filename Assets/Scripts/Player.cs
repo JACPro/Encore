00f564a5af8fc3f4623505b099bb95b585ff4c31
JACPro <jamesacpro@gmail.com>
2020-03-12 08:54:41 +0000

    Imported Aura 2, native American example scene, fire asset and produced demo scene for poster

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    string name;
+
+    public string getName() 
+    {
+        return name;        
+    }
+
+    public void setName(string name) 
+    {
+        this.name = name;
+    }
+}
---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public Quest quest;
+
+    public Player player;
+    
+}
